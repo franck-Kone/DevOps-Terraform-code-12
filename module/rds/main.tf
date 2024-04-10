71b7f58a536e13ce2f559406dc99c14b2386cd0d
banga <franckkone2807@gmail.com>
2024-04-09 22:14:54 -0400

    modules-built-process rds keypair provisioner variables w12d2

---
+++ module/rds/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    region = var.region
+  
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = var.allocated_storage
+  db_name              = var.db_name
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  username             = var.username
+  password             = var.password
+  parameter_group_name = var.parameter_group_name
+  skip_final_snapshot  = var.skip_final_snapshot
+  identifier = var.identifier
+}
+
+# resource "aws_db_instance" "default" {
+#   allocated_storage    = 10
+#   db_name              = "mydb"
+#   engine               = "mysql"
+#   engine_version       = "5.7"
+#   instance_class       = "db.t3.micro"
+#   username             = "foo"
+#   password             = "foobarbaz"
+#   parameter_group_name = "default.mysql5.7"
+#   skip_final_snapshot  = true
+#   identifier = "dev-database"
+# }
